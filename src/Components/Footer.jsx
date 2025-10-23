c8918d6b7ac7157377c363ababadcfb98869824d
deepak singla <singladeepak519@gmail.com>
2025-10-23 20:51:42 +0530

    comment added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,48 +1,61 @@
 import React from "react";
 
+// Importing CSS module for styling
 import Classes from "../Styles/Footer.module.css";
-import footerLogo from "../assets/footerlogo.jpg";
 
+// Importing images and icons
+import footerLogo from "../assets/footerlogo.jpg";
 import facebook from "../assets/facebook.svg";
 import github from "../assets/github.svg";
 import linkedin from "../assets/linkedin.svg";
 
+// Footer functional component
 function Footer() {
   return (
+    // Main footer container
     <footer className={Classes.footerContainer}>
       <div className={Classes.footer}>
+        {/* === Social Links Section === */}
         <div className={Classes.socialLink}>
+          {/* Contact number */}
           <p>9350317117</p>
+          {/* Travel name or username */}
           <p>Deepak_Travel</p>
+
+          {/* Social media icons */}
           <a href="">
-            <img src={facebook} alt="" />
+            <img src={facebook} alt="Facebook" />
           </a>
           <a href="">
-            <img src={linkedin} alt="" />
+            <img src={linkedin} alt="LinkedIn" />
           </a>
           <a href="">
-            <img src={github} alt="" />
+            <img src={github} alt="GitHub" />
           </a>
         </div>
 
+        {/* === Logo Section === */}
         <div className={Classes.footerLogo}>
+          {/* Clicking logo scrolls to hero section */}
           <a href="#hero">
-            <img src={footerLogo} alt="" />
+            <img src={footerLogo} alt="Footer Logo" />
             <p>
               Deepak <span>Travels</span>
             </p>
           </a>
         </div>
 
+        {/* === Subscription Section === */}
         <div className={Classes.footerInfo}>
           <h3>Explore the world from your inbox</h3>
           <p>
-            let us inspire your next gateway with new destinations and special
+            Let us inspire your next gateway with new destinations and special
             deals
           </p>
 
+          {/* Email input and subscribe button */}
           <div>
-            <input type="email" placeholder="email address" />
+            <input type="email" placeholder="Email address" />
             <button>Subscribe</button>
           </div>
         </div>
@@ -51,4 +64,5 @@ function Footer() {
   );
 }
 
+// Exporting the Footer component for use in other files
 export default Footer;
